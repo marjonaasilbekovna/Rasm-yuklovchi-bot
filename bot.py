fcfcc6d12d0dc2627dc6e0fd9bed54396d5fed48
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-09-26 15:56:22 +0300

    Cat and dog photo bot

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
-# 2-    usul----------------------------------------------------------------------
+# # 2-    usul----------------------------------------------------------------------
 
-# import requests
+import requests
 
-# cat = requests.get("https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_square.jpg")
-# dog = requests.get("https://www.youriowalawyers.com/images/blog/iStock-149075263.jpg")
+cat = requests.get("https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_square.jpg")
+dog = requests.get("https://www.youriowalawyers.com/images/blog/iStock-149075263.jpg")
 
-# animal = input("Hayvon monini kiriting: ")
+animal = input("Hayvon monini kiriting: ")
 
-# if animal == "cat":
-#     if cat.status_code == 200:
-#         content = cat.content
-#         with open("cat.jpg", "wb") as file:
-#             file.write(content)
-#             print("rasm yuklandi")
+if animal == "cat":
+    if cat.status_code == 200:
+        content = cat.content
+        with open("cat.jpg", "wb") as file:
+            file.write(content)
+            print("rasm yuklandi")
 
-# elif animal == "dog":
-#     if dog.status_code == 200:
-#         content = dog.content
-#         with open("dog.jpg", "wb") as file:
-#             file.write(content)
-#             print("rasm yuklandi")
-# else:
-#     print("Buday hayvon mavjud emas")
+elif animal == "dog":
+    if dog.status_code == 200:
+        content = dog.content
+        with open("dog.jpg", "wb") as file:
+            file.write(content)
+            print("rasm yuklandi")
+else:
+    print("Buday hayvon mavjud emas")
